50c9dbc9ea45c86a65d2d3e176097475b987faa2
leon <leon_zmq520@hotmail.com>
2014-12-09 11:23:03 +0100

    update app

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
   console.log("ready!");
   //1 - Afficher les ingrédients de la pizza au survol de leurs nom
-  $(".radio").click(function(){
-    console.log("select pizza type radio button");
-    //if($(this).children("input:checked")){
-      //console.log($( "input:checked" ).val() + " is checked!" );
-    $(this).children(".description.no-display").show(500);
-    //}
+  $(".radio").hover(function(){
+    console.log("mouse move at pizza type radio button");
+      $(this).children(".description.no-display").show(300);
+    },function(){
+      $(this).children(".description.no-display").hide();
   });
 
   //2 - Afficher visuellement le nombre de parts et le nombre de pizzas approprié en dessous du champs « Nombre de parts »
